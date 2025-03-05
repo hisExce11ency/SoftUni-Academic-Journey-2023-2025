326658e5c751b4093c07789100928910a2074057
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2025-03-05 15:18:02 +0200

    Update dateTimeUtils.js

--- ReactJS-February-2025/04-Workshop-Components/React-JS-User-List-Workshop/client/src/utils/dateTimeUtils.js
+++ ReactJS-February-2025/04-Workshop-Components/React-JS-User-List-Workshop/client/src/utils/dateTimeUtils.js
@@ -1,10 +1,8 @@
 export const formatDate = (isoDate) => {
+
     const date = new Date(isoDate);
-    const formattedDate = date.toLocaleDateString('en-GB', {
-        day: 'numeric',
-        month: 'short',
-        year: 'numeric',
-    });
+    const dateOptions = { day: 'numeric', month: 'short', year: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-GB', dateOptions);
 
     return formattedDate;
 };
