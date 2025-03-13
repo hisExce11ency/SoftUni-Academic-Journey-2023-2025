45d41abd1ec1bf1fb56c45767f88595d38113ce0
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2025-03-13 21:48:32 +0200

    Created Product component

--- ReactJS-February-2025/06-Routing/RoutingDemo/src/components/Poducts.jsx
+++ ReactJS-February-2025/06-Routing/RoutingDemo/src/components/Poducts.jsx
@@ -1,3 +1,5 @@
+import Product from "./Product";
+
 const products = [
     {
         id: 1,
@@ -63,23 +65,7 @@ export default function Catalog() {
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 text-center">
                 <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
-                        <a
-                            key={product.id}
-                            href={product.href}
-                            className="group"
-                        >
-                            <img
-                                alt={product.imageAlt}
-                                src={product.imageSrc}
-                                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
-                            />
-                            <h3 className="mt-4 text-sm text-gray-700">
-                                {product.name}
-                            </h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">
-                                {product.price}
-                            </p>
-                        </a>
+                        <Product key={product.id} product={product} />
                     ))}
                 </div>
             </div>
