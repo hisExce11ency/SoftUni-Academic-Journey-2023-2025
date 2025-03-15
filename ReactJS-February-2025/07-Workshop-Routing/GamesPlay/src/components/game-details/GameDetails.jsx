de5b61c9a3af7105ccca97d27745eeedca023b72
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2025-03-16 01:09:09 +0200

    Extract comments related jsx in separate components

--- ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/components/game-details/GameDetails.jsx
+++ ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/components/game-details/GameDetails.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
 import gameService from "../../services/gameService";
+import DisplayComents from "../comments-display/DisplayComments";
+import AddComent from "../add-comments/AddComment";
 
 {
     /*
@@ -42,27 +44,7 @@ export default function GameDetails() {
 
                 <p className="text">{game.summary}</p>
 
-                {/*
-        <!-- Bonus ( for Guests and Users ) -->
-        */}
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        {/*
-                <!-- list all comments for current game (If any) -->
-                */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
-                    </ul>
-                    {/*
-            <!-- Display paragraph: If there are no games in the database -->
-            */}
-                    <p className="no-comment">No comments.</p>
-                </div>
+                <DisplayComents />
 
                 {/*
         <!-- Edit/Delete buttons ( Only for creator of this game )  -->
@@ -76,26 +58,7 @@ export default function GameDetails() {
                     </button>
                 </div>
             </div>
-            {/*
-    <!-- Bonus -->
-    */}{" "}
-            {/*
-    <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
-    */}
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form">
-                    <textarea
-                        name="comment"
-                        placeholder="Comment......"
-                    ></textarea>
-                    <input
-                        className="btn submit"
-                        type="submit"
-                        value="Add Comment"
-                    />
-                </form>
-            </article>
+            <AddComent />
         </section>
     );
 }
