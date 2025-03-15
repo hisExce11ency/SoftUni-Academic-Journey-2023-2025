731cd23388a27761e8155540a8ca62b7071dde59
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2025-03-15 16:09:17 +0200

    Add Login

--- ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/App.jsx
+++ ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router";
 
 import Header from "./components/header/Header";
+
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
 
 import "./App.css";
 function App() {
@@ -11,6 +13,7 @@ function App() {
             {/* <!-- Main Content -->*/}
             <main id="main-content">
                 <Routes>
+                    <Route path="/login" element={<Login />} />
                     <Route index element={<Home />} />
                 </Routes>
             </main>
