84c139545edd3f22e1b6bb654877b17f2e4e8e69
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2025-03-15 16:33:30 +0200

    Add GameCreate

--- ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/App.jsx
+++ ReactJS-February-2025/07-Workshop-Routing/GamesPlay/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import GameCreate from "./components/game-create/GameCreate";
 
 import "./App.css";
 function App() {
@@ -17,6 +18,7 @@ function App() {
                     <Route index element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/games/create" element={<GameCreate />} />
                 </Routes>
             </main>
         </div>
